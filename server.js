9ebfd6021d005b3143f54444fc14e37f00d1dd5d
joshfarhi <joshfarhi@icloud.com>
2019-02-20 11:31:18 -0500

    started hw

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+var app = express();
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+    "Grabbing every thread name and link\n" +
+    "from reddit's fortnite subreddit:" +
+    "\n***********************************\n");
+
+// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+axios.get("https://old.reddit.com/r/fortnitebr/").then(function (response) {
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // With cheerio, find each p-tag with the "title" class
+    // (i: iterator. element: the current element)
+    $("p.title").each(function (i, element) {
+
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+        var link = $(element).children().attr("href");
+
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+            title: title,
+            link: link
+        });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
+});
